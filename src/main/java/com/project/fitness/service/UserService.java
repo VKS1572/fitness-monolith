71ce9da5a1d5281c5896295d8254c6c461289127
VKS1572 <vikaspradhanrisora@gmail.com>
2025-12-27 16:05:39 +0530

    Request mapping and api uses..

---
+++ src/main/java/com/project/fitness/service/UserService.java
@@ -0,0 +1,20 @@
+package com.project.fitness.service;
+
+import com.project.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import com.project.fitness.model.User;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
+        private final UserRepository userRepository;
+
+        public User register (User user){
+           return userRepository.save(user);
+        }
+}
+
